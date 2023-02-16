a54135d2b99addbe9e6100247a3ce77a85459f7a
Kook16 <jamesss075@gmail.com>
2023-02-16 15:41:42 +0000

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
 
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(2, str, 59);
+	write(2, str, 59);
 	return (1);
 }
