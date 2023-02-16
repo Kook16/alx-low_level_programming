f4a1fca7bb047de3df8a5764185baf079290483b
Kook16 <jamesss075@gmail.com>
2023-02-16 13:27:59 +0000

    Adde new files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print - Message
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
