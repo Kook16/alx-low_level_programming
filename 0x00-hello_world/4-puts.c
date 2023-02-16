c829f0d148e029d7a20304406186d60264027bf0
Kook16 <jamesss075@gmail.com>
2023-02-16 12:55:33 +0000

    Added new files and fixes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts: Prints a Sentence with a new line
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programing is like building a multilingual puzzel");
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,2 @@
+# 0-processor 
+   - preprocessor stage
