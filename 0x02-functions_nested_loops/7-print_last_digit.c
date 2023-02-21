1738d6732dd689d054a8c9a7f0c2ce647d4f99f2
Kook16 <jamesss075@gmail.com>
2023-02-21 17:21:24 +0000

    Fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,13 @@ int print_last_digit(int n)
 	int i;
 
 	if (n >= 0)
+	{
 		i = n % 10;
+	}
 	else
+	{
 		i = (n * -1) % 10;
+	}
+	_putchar(i + '0');
 	return (i);
 }
--- 0x02-functions_nested_loops/7-print_last_digit.c~
+++ 0x02-functions_nested_loops/7-print_last_digit.c~
@@ -10,6 +10,9 @@ int print_last_digit(int n)
 {
 	int i;
 
-	i = n % 10;
+	if (n >= 0)
+		i = n % 10;
+	else
+		i = (n * -1) % 10;
 	return (i);
 }
