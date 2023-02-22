aeff0b3d4bcc3984e422f62c2ab59b5d4e569af2
Kook16 <jamesss075@gmail.com>
2023-02-22 09:01:06 +0000

    fix

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -6,7 +6,7 @@ int main(void)
 	int a,b,c;
 	int largest;
 
-	a = 972, b = -98, c = 0;
+	a = -972, b = -98, c = 0;
 	largest = largest_number(a,b,c);
 	printf("%d\n", largest);
 	return 0;
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,4 +5,5 @@
 
 void positive_or_negative(int);
 int largest_number(int, int, int);
+
 #endif
