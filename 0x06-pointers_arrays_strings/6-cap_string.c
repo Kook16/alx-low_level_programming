9f807794ba240d0621a36b26b87a798a61dcef87
Kook16 <jamesss075@gmail.com>
2023-03-02 05:31:43 +0000

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,12 +15,13 @@ char *cap_string(char *s)
 	n = strlen(s);
 	while (i < n)
 	{
+		if (s[0] >= 97 && s[0] >= 122)
+			s[0] = toupper(s[0]);
 		if (s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!'
 || s[i] == '?' || s[i] == '"' || s[i] == ')' || s[i] == '(' ||
 s[i] == '{' || s[i] == '}' || s[i] == '\n' || s[i] == ' ' || s[i] == '\t')
 			s[i + 1] = toupper(s[i + 1]);
 		i++;
 	}
-	i--;
 	return (s);
 }
