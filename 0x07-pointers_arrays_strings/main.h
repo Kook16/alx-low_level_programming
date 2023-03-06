19467d4a5d29029c85d83c136d9766e8ad75653f
Kook16 <jamesss075@gmail.com>
2023-03-06 18:23:14 +0000

    new

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void set_string(char **, char *);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
