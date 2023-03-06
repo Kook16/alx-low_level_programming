05c73e0adc94075260365769f2dde1fd0446aeeb
Kook16 <jamesss075@gmail.com>
2023-03-06 12:58:09 +0000

    fix

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: Address of the first character of tthe string
+ * @accept: Address of the first character of string2
+ * Return: a character pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *p = accept
+
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+			{
+				return (s);
+			}
+			accept++;
+		}
+		s++;
+		accept = p;
+	}
+	return (NULL);
+}
