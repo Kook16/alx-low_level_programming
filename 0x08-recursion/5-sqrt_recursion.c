bfbb292dc6d2d6c501d2eef584950650244b85ea
Kook16 <jamesss075@gmail.com>
2023-03-08 11:23:21 +0000

    fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,12 +5,15 @@
  * @n: Number whose natural square is required
  * Return: Integer
  */
-
 int _sqrt_recursion(int n)
 {
-	int m = sqrt(n);
-	if (m * m != n)
+	int i = 1;
+
+	if (n < 0)
 		return (-1);
-	else
-		return (m);
+
+	if (n <= 1)
+		return (n);
+	return (sqt(n, i, n));
 }
+
