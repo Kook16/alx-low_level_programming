24055a0f7b97dbae1d470af1b21222935cff2de4
Kook16 <jamesss075@gmail.com>
2023-03-10 09:55:34 +0000

    fix

--- 0x0A-argc_argv/4.add.c
+++ 0x0A-argc_argv/4.add.c
@@ -19,9 +19,14 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i] >= 48 && argv[i] <= 57)
+		if (atoi(argv[i]))
 		{
 			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
 		}
 	}
 	printf("%d\n", sum);
