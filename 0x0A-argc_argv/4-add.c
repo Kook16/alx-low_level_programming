ef1a09c603478fd5ecd7175e96269af0f60ed57f
Kook16 <jamesss075@gmail.com>
2023-03-10 11:14:35 +0000

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 
 	while (i < argc)
 	{
-		if (atoi(argv[i]) && atoi(argv[i]) >= 0)
+		if (atoi(argv[i]) && atoi(argv[i]) > 0)
 		{
 			break;
 		}
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) && atoi(argv[i]) >= 0)
+			if (atoi(argv[i]) && atoi(argv[i]) > 0)
 			{
 				sum += atoi(argv[i]);
 			}
