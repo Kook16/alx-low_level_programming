be1b2b75beec72fa822703b67e6726c7f5df4b20
Kook16 <jamesss075@gmail.com>
2023-03-16 18:52:18 +0000

    fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (malloc(new_size));
 	}
 
-	if (new_size <= old_size)
+	if (new_size == old_size)
 	{
 		return (ptr);
 	}
