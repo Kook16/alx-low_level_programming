fc82acf3bdd156eea615454d8c9f0990771d8216
Kook16 <jamesss075@gmail.com>
2023-03-16 18:46:34 +0000

    fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,8 +29,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
-
 	new_ptr = malloc(new_size);
+	if (new_size > old_size)
+		return (new_ptr);
 	memcpy(new_ptr, ptr, old_size);
 	return (new_ptr);
 }
