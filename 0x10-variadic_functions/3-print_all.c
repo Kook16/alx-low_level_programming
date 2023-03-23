53a7b30c77a54410243fa823e7dc1134ef551619
Kook16 <jamesss075@gmail.com>
2023-03-23 15:49:15 +0000

    FIX

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,50 +4,66 @@
  * print_all - prints anything
  * @format: ...
  */
-
 void print_all(const char *const format, ...)
 {
 	int n, i = 0, j = 0;
 	va_list ap;
-	char *s;
 	char *seperator = "";
 
 	va_start(ap, format);
 	n = strlen(format);
+
 	while (j < n && format != NULL)
 	{
-		i = 0;
-		switch (format[j])
-		{
-		case 'c':
-			printf("%s%c", seperator, va_arg(ap, int));
-			i = 1;
-			break;
-		case 'i':
-			printf("%s%d", seperator, va_arg(ap, int));
-			i = 1;
-			break;
-		case 'f':
-			printf("%s%f", seperator, va_arg(ap, double));
-			i = 1;
-			break;
-		case 's':
-			i = 1;
-			s = va_arg(ap, char *);
-			if (s == NULL)
-			{
-				printf("%s%s", seperator, "(nil)");
-				break;
-			}
-			printf("%s%s", seperator, s);
-			break;
-		default:
-			break;
-		}
-		if (j != (n - 1) && i == 1)
+
+		print_argument(format[j], ap, seperator, &i);
+		if (i != (n - 1) && i == 1)
 			printf(", ");
+		i = 0;
 		j++;
 	}
 	va_end(ap);
 	printf("\n");
+}
+
+
+/**
+ * print_argument - .....
+ * @format: ...
+ * @ap: ...
+ * @seperator: ...
+ * @i: ...
+ */
+
+void print_argument(char format, va_list ap, char *seperator, int *i)
+{
+	char *s;
+
+	switch (format)
+	{
+	case 'c':
+		printf("%s%c", seperator, va_arg(ap, int));
+		*i = 1;
+		break;
+	case 'i':
+		printf("%s%d", seperator, va_arg(ap, int));
+		*i = 1;
+		break;
+	case 'f':
+		printf("%s%f", seperator, va_arg(ap, double));
+		*i = 1;
+		break;
+	case 's':
+		*i = 1;
+		s = va_arg(ap, char *);
+		if (s == NULL)
+		{
+			printf("%s%s", seperator, "(nil)");
+			break;
+		}
+		printf("%s%s", seperator, s);
+		break;
+	default:
+		break;
+	}
 }
---
+++ 0x10-variadic_functions/test.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_all("ceissic", 'B', 3, "stSchool", NULL, 10002, 'g');
+	return (0);
+}
