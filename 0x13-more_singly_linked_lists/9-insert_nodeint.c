b35d15c38157e7e226d7947f4144f18a90b13745
Kook16 <jamesss075@gmail.com>
2023-04-04 12:46:41 +0000

    FIX 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,14 +14,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0, count;
 
 	count = count_node(*head);
+	if (idx > count)
+		return (NULL);
 	new = add(n);
 	if (new == NULL)
 		return (NULL);
-	if (idx >= count)
-	{
-		free(new);
-		return (NULL);
-	}
 	if (*head == NULL)
 	{
 		*head = new;
