7d696b9f706dba8d24cdba06772f59cc79590b47
Kook16 <jamesss075@gmail.com>
2023-04-04 04:19:09 +0000

    FIX

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,5 +15,6 @@ void free_listint2(listint_t **head)
 		aux = aux->next;
 		free(ptr);
 	}
+	free(aux);
 	*head = NULL;
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include "lists1.h"
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
