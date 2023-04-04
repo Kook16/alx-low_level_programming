8d3f6c3194979982d7f0ee0f968023ea0e9d0e6c
Kook16 <jamesss075@gmail.com>
2023-04-04 02:40:04 +0000

    FIX

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,14 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		return (count);
-	while (h != NULL)
+	if (h != NULL)
 	{
-		printf("%d\n", h->n);
-		count++;
-		h = h->next;
+		while (h != NULL)
+		{
+			printf("%d\n", h->n);
+			count++;
+			h = h->next;
+		}
 	}
 	return (count);
 }
