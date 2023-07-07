7cd9668e85a46c3df1628195fee3b312f0070ecb
Kook16 <jamess075@gmail.com>
2023-07-07 08:48:51 +0000

    initialized all array to NULL instead of 0 after the call to calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,11 +9,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ptr_2_ht;
+	unsigned int i = 0;
 
 	if (size <= 0)
 		return (NULL);
 	ptr_2_ht = calloc(size, sizeof(hash_table_t));
 	if (ptr_2_ht == NULL)
 		return (NULL);
+	for (i = 0; i < size - 1; i++)
+		(ptr_2_ht + 1)->array = NULL;
 	return (ptr_2_ht);
 }
