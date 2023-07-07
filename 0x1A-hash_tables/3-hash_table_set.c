03b9d91bb98db4428f94d062f6d2073a696de543
Kook16 <jamess075@gmail.com>
2023-07-07 19:42:27 +0000

    Added task 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+hash_node_t *create_node(const char *key, const char *value);
 
 /**
  * hash_table_set - adds an element to the hash table
@@ -9,33 +10,63 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_table_t *ptr_t;
-	/* struct representing node of hash table */
-	hash_node_t *ptr_node, *head;
+	hash_node_t *ptr_node, *ptr_t;
 	unsigned long int idx;
 	const unsigned char *str_key = (const unsigned char *)key;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
 		return (0);
-	if (strcmp(key, "") == 0)
-		return (0);
-	ptr_t = ht;
-	idx = key_index(str_key, ptr_t->size);
-	ptr_node = malloc(sizeof(hash_node_t));
-
+	idx = key_index(str_key, ht->size);
+	ptr_node = create_node(key, value);
 	if (ptr_node == NULL)
 		return (0);
-	ptr_node->key = strdup(key);
-	ptr_node->value = strdup(value);
-	ptr_node->next = NULL;
-
-	if (ptr_t->array[idx] == NULL)
-		ptr_t->array[idx] = ptr_node;
-	else
+	ptr_t = ht->array[idx];
+	if (ptr_t != NULL)
 	{
-		head = ptr_t->array[idx];
-		ptr_node = head;
-		head = ptr_node;
+		if (strcmp(ptr_t->key, key) == 0)
+		{
+			free(ptr_t->value);
+			ptr_t->value = strdup(value);
+			free(ptr_node->key);
+			free(ptr_node->value);
+			free(ptr_node);
+			return (1);
+		}
+		ptr_t = ptr_t->next;
 	}
+	ptr_node->next = ht->array[idx];
+	ht->array[idx] = ptr_node;
 	return (1);
+}
+
+/**
+ * create_node - creates the items of the hash table
+ * @key: String representing key
+ * @value: string representing value
+ * Return: A pointer to a new hash node item
+ */
+
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(node));
+
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->value);
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
 }
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table you want to look into
+ * @key: the key you are looking for
+ * Return: the value associated with the element, or NULL if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *hash_node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	idx = key_index((const unsigned char *)(key), ht->size);
+	hash_node = ht->array[idx];
+	while (hash_node != NULL)
+	{
+		if (strcmp(hash_node->key, key) == 0)
+			return (hash_node->value);
+		hash_node = hash_node->next;
+	}
+	return (NULL);
+}
