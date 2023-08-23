37e632b9d42ad6337bea39473c0e8aad449f3d8d
Kook16 <jamesss075@gmail.com>
2023-08-23 23:24:22 +0300

    Added task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Defining a island_perimeter function'''
+
+
+def island_perimeter(grid):
+    '''This function returns the perimeter of the island'''
+
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
